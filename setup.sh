a0a88f0df9dee6b44af4f8e839926d6ebdef1b78
JD <jan-david.fischbach@rwth-aachen.de>
2021-09-14 17:14:16 +0000

    modify setup => dont setup docker for everyone

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 apt-get update
-apt-get install \
+apt-get install -y \
     apt-transport-https \
     ca-certificates \
     curl \
@@ -12,10 +12,7 @@ echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] 
   $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 apt-get update
-apt-get install docker-ce docker-ce-cli containerd.io
-groupadd docker
-usermod -aG docker $USER
-newgrp docker 
+apt-get install -y docker-ce docker-ce-cli containerd.io
 
 curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
