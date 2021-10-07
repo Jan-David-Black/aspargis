d1fc5f2484f6df365aa804e926808f12c7552098
JD <jan-david.fischbach@rwth-aachen.de>
2021-10-07 13:34:36 +0000

    work on pi

--- setup.sh
+++ setup.sh
@@ -1,61 +1,83 @@
-#!/bin/bash 
+#!/bin/bash
+usage(){
+    echo "sudo ./setup.sh -<h|d|f|n>"
+    echo "-h this help"
+    echo "-d restart docker-compose"
+    echo "-f force install (overwrites .env)"
+    echo "-n no install (even if .env doesn't exist yet)"
+}
 
-if [ "$EUID" -ne 0 ]
-  then echo "Please run as root"
-  exit
+while getopts "hdfns" arg; do
+  case $arg in
+    d) DockerRestart=true;;
+    f) Force=true;;
+    n) NoInstall=true;;
+    h) Help=true;;
+    s) ServerInstall=true;;
+  esac
+done
+
+if [ "$Help" = "true" ]; then
+    usage
+    exit
 fi
 
+if [ ! -f .env ]||[ "$Force" = "true" ]&&[ ! "$NoInstall" = "true" ]; then
+    if [ "$EUID" -ne 0 ];
+        then echo "Please run as root"
+        exit
+    fi
 
-if [ ! -f .env ]; then
     echo "SECRET=$(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c${1:-32})">.env
     echo "POSTGRESSPWD=$(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c${1:-32})">>.env
 
     cp .env populate/
-    cp .env time-series-chart/
-    sed -i 's/SECRET/REACT_APP_SECRET/' time-series-chart/.env
 
-    apt-get update
-    apt-get install -y \
-        apt-transport-https \
-        ca-certificates \
-        curl \
-        gnupg \
-        lsb-release
+    curl -fsSL https://get.docker.com -o get-docker.sh
+    sh ./get-docker.sh
 
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+    sudo apt install -y python3-pip libffi-dev
+    sudo pip3 install docker-compose
 
-    echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
-    $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+    sudo usermod -aG docker $SUDO_USER
 
-    apt-get update
-    apt-get install -y docker-ce docker-ce-cli containerd.io
+#TODO select right yaml
 
-    echo "granting docker privileges to $SUDO_USER"
-    usermod -aG docker $SUDO_USER
-
-    curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-    chmod +x /usr/local/bin/docker-compose
-    
-    su $SUDO_USER -c"
-        curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
-        export NVM_DIR=\"\$HOME/.nvm\"
-        [ -s \"\$NVM_DIR/nvm.sh\" ] && \. \"\$NVM_DIR/nvm.sh\"
-        echo \$HOME
-        nvm install node
-        if [ ! -d time-series-chart/node_modules ]; then
-            cd time-series-chart
-            npm install
-            cd ..
-        fi 
-
-        if [ ! -d populate/node_modules ]; then
-            cd populate
-            npm install
-            cd ..
-        fi 
-        "
+    if [ $(dpkg --print-architecture) == amd64 ]; then
+        echo "usual procedure (not a  pi)"
+    else
+        echo "assuming its a pi"
+    fi
 fi
 
-docker kill $(docker ps -q)
-docker rm $(docker ps -a -q)
-docker-compose up -d
+if [ "$ServerInstall" = "true" ]; then
+    if [ "$EUID" -ne 0 ];   
+        then echo "Please run as root"
+        exit
+    fi
+    echo "installing node and running npm i"
+    su $SUDO_USER -c"
+            curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
+            export NVM_DIR=\"\$HOME/.nvm\"
+            [ -s \"\$NVM_DIR/nvm.sh\" ] && \. \"\$NVM_DIR/nvm.sh\"
+            echo \$HOME
+            nvm install node
+            if [ ! -d aspargis/node_modules ]; then
+                cd aspargis
+                npm install
+                cd ..
+            fi 
+
+            if [ ! -d populate/node_modules ]; then
+                cd populate
+                npm install
+                cd ..
+            fi 
+            "
+fi
+
+if [ "$DockerRestart" = "true" ]; then
+    echo "restarting docker-compose"
+    docker-compose down
+    docker-compose up -d
+fi
