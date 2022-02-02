540c0411aad7303d81ab84db0c0c7d3264465ca1
JD <jan-david.fischbach@rwth-aachen.de>
2022-02-02 08:39:31 +0100

    Modify testing script for new message format

--- test/test.py
+++ test/test.py
@@ -6,29 +6,47 @@ from aiocoap import *
 
 import argparse
 
+def check_positive(value):
+    ivalue = int(value)
+    if ivalue <= 0:
+        raise argparse.ArgumentTypeError("%s is an invalid positive int value" % value)
+    return ivalue
+    
 parser = argparse.ArgumentParser(description='Contact COAP server')
 parser.add_argument('address', metavar='A',
                     help='Server Address')
                     
 parser.add_argument('sgroup', metavar='S',
                     help='SGroup ID')
+              
+parser.add_argument('-n', action='store_true')
+
+parser.add_argument('-r', default=1, type=check_positive)
                     
 args = parser.parse_args()
 
 logging.basicConfig(level=logging.INFO)
+
+
 payload = b'{"payload": [{"Date": "", "Values": {"temp1": 22.3750, "temp2": 18.2500, "temp3": 18.6875, "temp4": 18.8125, "v_bat": 3247}}], "signal": 15}'
+if args.n:
+	payload = b'41604255471800780cd11404001f000000000000000061f39071'
+	print("Using new Messgae Protocoll")
+
 async def main():
+
     protocol = await Context.create_client_context()
 
     request = Message(code=GET, uri='coap://'+args.address+':5683/mqtt/'+args.sgroup+'/', payload=payload)
 
     try:
-        response = await protocol.request(request).response
+	    response = await protocol.request(request).response
     except Exception as e:
-        print('Failed to fetch resource:')
-        print(e)
+	    print('Failed to fetch resource:')
+	    print(e)
     else:
-        print('Result: %s\n%r'%(response.code, response.payload))
+	    print('Result: %s\n%r'%(response.code, response.payload))
 
 if __name__ == "__main__":
-    asyncio.get_event_loop().run_until_complete(main())
+    for i in range(args.r):
+        asyncio.get_event_loop().run_until_complete(main())
